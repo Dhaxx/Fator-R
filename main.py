7b19ab124ddfe2cc5688ad0335aa47948104d492
Dhaxx <kaiopablo44@gmail.com>
2026-03-26 16:34:27 -0300

    Add new image files and Jupyter notebook for data analysis
    - Added 'empresa_input.png', 'empresas.png', and 'nenhuma.png' image files.
    - Created 'teste.ipynb' Jupyter notebook for data analysis, including data loading and processing.
    - Implemented calculations for 'valor_faturamento_nfse' and 'inss_mes_anterior' in the notebook.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 from PIL import ImageGrab
 from pathlib import Path
 import shutil
+import pandas as pd
 
 def clicar_imagem(img, tentativas=5, confidence=0.8):
     for i in range(tentativas):
@@ -13,6 +14,13 @@ def clicar_imagem(img, tentativas=5, confidence=0.8):
             return True
         time.sleep(1)
     return False
+
+def gerar_novos_salarios():
+    df = pd.read_excel(f'{pasta_fator_r}/Fator R - {empresas}.xls', engine='xlrd')
+    valores = ((df["valor_faturamento_nfse"] * 0.28) - df["inss_mes_anterior"]).tolist()
+    valores_formatados = [f"{v:.2f}".replace(".", ",") for v in valores]
+    return valores_formatados
+
 
 # Cria a pasta "Fator R" na raiz do disco onde este script está
 pasta_raiz_disco = Path(__file__).resolve().anchor
@@ -31,28 +39,28 @@ screenshot = ImageGrab.grab(all_screens=True)
 screenshot.save('todos_monitores.png')
 
 print("===[Variáveis para o Cálculo do Fator R]===")
-empresas = '1104, 1105, 1106'#input("Digite o(s) código(s) (separados por vírgula): ")
+empresas = '323,326,327'#input("Digite o(s) código(s) (separados por vírgula): ")
 dt_inicio = '12/2025'#input("Competência inicial (MM/AAAA): ")
 dt_fim = '12/2025'#input("Competência final (MM/AAAA): ")
 
 print("===[Variável para a alteração salarial]===")
-dt_alteracao = '31/12/2025'#input("Data da alteração salarial (DD/MM/AAAA): ")
+dt_alteracao = '31/12/2026'#input("Data da alteração salarial (DD/MM/AAAA): ")
 
 # chamada da função
 if clicar_imagem("favoritos.png"):
     print("Clicou em Favoritos")
     pyautogui.press('down')
     pyautogui.press('enter')
-    time.sleep(1)
+    time.sleep(3)
     pyautogui.write('Relatórios')
-    time.sleep(1)
+    time.sleep(2)
     pyautogui.write('Diversos')
     pyautogui.press('enter')
     pyautogui.write('Cálculo Fator R')
     pyautogui.press('enter')
     time.sleep(2)
     
-    clicar_imagem("campo1.png")
+    pyautogui.doubleClick(pyautogui.locateOnScreen("campo1.png", confidence=0.8))
     pyautogui.write(empresas)
 
     pyautogui.press('tab')
@@ -69,6 +77,7 @@ if clicar_imagem("favoritos.png"):
     pyautogui.press('p')
     time.sleep(2)
     clicar_imagem("export_opt.png")
+    pyautogui.press('enter')
     time.sleep(2)
     clicar_imagem("desktop.png")
     time.sleep(2)
@@ -100,7 +109,42 @@ if clicar_imagem("favoritos.png"):
     pyautogui.write('ALTERACAO')
     pyautogui.press('tab', presses=2)
     pyautogui.press('down')
-    pyautogui.press('tab', presses=8)
+    time.sleep(2)
+    clicar_imagem("empresas.png")
+    time.sleep(0.1)
+    # pyautogui.press('enter')                                
 
+    time.sleep(2)
+
+    clicar_imagem("nenhuma.png")
+    pyautogui.doubleClick(pyautogui.locateOnScreen("empresa_input.png", confidence=0.8))
+
+    for empresa in empresas.split(','):
+        pyautogui.write(empresa.strip())
+        pyautogui.press('tab', presses=8)
+        pyautogui.hotkey('shift','space')
+
+        for i in range(8):
+            pyautogui.hotkey('shift', 'tab')
+            time.sleep(0.1) # Aguarda 100ms entre cada tab
+
+        pyautogui.doubleClick(pyautogui.locateOnScreen("empresa_input.png", confidence=0.8))
+
+    pyautogui.press('tab')
+    pyautogui.press('enter')
+    
+    for i in range(2):
+        pyautogui.hotkey('shift', 'tab')
+        time.sleep(0.1) # Aguarda 100ms entre cada tab
+    
+    pyautogui.press('enter')
+    pyautogui.press('enter')
+    time.sleep(2)
+
+    valores = gerar_novos_salarios()
+
+    for valor in valores:
+        pyautogui.write(str(valor))
+        pyautogui.press('tab')
 else:
     print("Não encontrou a imagem")
