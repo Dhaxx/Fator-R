999962131316a0963d0611dabf3ad5a01c5c4a0a
Dhaxx <kaiopablo44@gmail.com>
2026-03-26 00:04:12 -0300

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pyautogui
 import time
 from PIL import ImageGrab
+from pathlib import Path
+import shutil
 
 def clicar_imagem(img, tentativas=5, confidence=0.8):
     for i in range(tentativas):
@@ -12,17 +14,32 @@ def clicar_imagem(img, tentativas=5, confidence=0.8):
         time.sleep(1)
     return False
 
+# Cria a pasta "Fator R" na raiz do disco onde este script está
+pasta_raiz_disco = Path(__file__).resolve().anchor
+pasta_fator_r = Path(pasta_raiz_disco) / "Fator R"
+pasta_fator_r.mkdir(parents=True, exist_ok=True)
+
+# Limpa todo o conteúdo da pasta "Fator R" (arquivos e subpastas)
+for item in pasta_fator_r.iterdir():
+    if item.is_dir():
+        shutil.rmtree(item)
+    else:
+        item.unlink()
+
 # Detecta todos os monitores
 screenshot = ImageGrab.grab(all_screens=True)
 screenshot.save('todos_monitores.png')
 
-empresas = input("Digite o(s) código(s) (separados por vírgula): ")
+empresas = '1104, 1105, 1106'#input("Digite o(s) código(s) (separados por vírgula): ")
+dt_inicio = '12/2025'#input("Competência inicial (MM/AAAA): ")
+dt_fim = '12/2025'#input("Competência final (MM/AAAA): ")
 
 # chamada da função
 if clicar_imagem("favoritos.png"):
     print("Clicou em Favoritos")
     pyautogui.press('down')
     pyautogui.press('enter')
+    time.sleep(2)
     pyautogui.write('Relatórios')
     time.sleep(2)
     pyautogui.write('Diversos')
@@ -31,10 +48,30 @@ if clicar_imagem("favoritos.png"):
     pyautogui.press('enter')
     time.sleep(2)
     
-    # Mantem Ctrl e Shift pressionados enquanto envia Right para selecionar.
-    # segura Ctrl e Shift
-    pyautogui.keyDown('ctrl')
-    pyautogui.keyDown('shift')
-    pyautogui.press('right')
+    clicar_imagem("campo1.png")
+    pyautogui.write(empresas)
+
+    pyautogui.press('tab')
+    pyautogui.write(dt_inicio)
+    pyautogui.press('tab')
+    pyautogui.write(dt_fim)
+
+    clicar_imagem("executar1.png")
+    time.sleep(5)
+    pyautogui.press('enter')
+    time.sleep(4)
+    clicar_imagem("exportar.png")
+    pyautogui.press('enter')
+    time.sleep(2)
+    clicar_imagem("desktop.png")
+    clicar_imagem("campo2.png")   
+    pyautogui.write(f'Client {pasta_raiz_disco[0]}')
+    print(f"Salvando em: Client {pasta_raiz_disco[0]}")
+    pyautogui.press('enter')
+    time.sleep(2)
+    pyautogui.doubleClick(pyautogui.locateOnScreen("pasta.png", confidence=0.8))
+    clicar_imagem("save.png")
+    time.sleep(2)
+    pyautogui.press('escape')
 else:
     print("Não encontrou a imagem")
